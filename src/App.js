fbb9baf2cb714d495d9b0786a340cbbc04e7858a
jefinsathyekkal <jefinbasheer786@gmai.com>
2024-01-07 10:45:24 +0530

    props the value is passing from parent file to child

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 
-
+import './app.css';
+import Header from './Heder';
 function App() {
+  const data='jefin sathyekkal'
   return (
-  <h1>hello world</h1>
+    <div>
+  
+  <Header data={data}/>
+  
+  <p>hai iam jefin sathyekkla house apdi{data}</p>
+ 
+
+  </div>
   );
 }
 
 export default App;
+
