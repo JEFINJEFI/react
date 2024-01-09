3f7aafd7743fbcd25751c0e9cde97fda3f274a7e
jefinsathyekkal <jefinbasheer786@gmai.com>
2024-01-09 22:13:31 +0530

    employers name and age list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Counter from "./components/counter";
+import Employee from "./components/Employee";
 function App() {
 
   const [count,setCount]= useState(0)
@@ -10,17 +10,25 @@ function Addc(){
   setCount(count+1)
   console.log(count);
 }
-let obj={
-  title:'first counter',
-  count
-}
+let emp =[
+ {name:'jefin',age:20},
+  {name:'agil',age:20},
+  {name:'sreevidhya',age:20},
+  
+]
   return (
      
     <div>
       <button onClick={Addc}>Add</button>
-     <Counter {...obj}/>
-     <Counter title='2nd conter' count={count}/>
-     <Counter title='third' count={count}/>
+      {
+        emp.map((obj)=>{
+          return(
+            <Employee name={obj.name} age={obj.age}/>
+          )
+
+        })
+      }
+    
   </div>
   );
 }
